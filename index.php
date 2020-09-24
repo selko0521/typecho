cd3a4d79ec8fa585c5d9a08c95877982e0a79c6f
selko0521 <32323180+selko0521@users.noreply.github.com>
2020-09-24 10:57:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
-<?php
+<？php
 /**
- * Typecho Blog Platform
+ * Typecho博客平台
  *
- * @copyright  Copyright (c) 2008 Typecho team (http://www.typecho.org)
- * @license    GNU General Public License 2.0
- * @version    $Id: index.php 1153 2009-07-02 10:53:22Z magike.net $
+ * @copyright版權所有（c）2008 Typecho團隊（http://www.typecho.org）
+ * @許可證GNU通用公共許可證2.0
+ * @version $ Id：index.php 1153 2009-07-02 10：53：22Z magike.net $
  */
 
-/** 载入配置支持 */
-if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once 'config.inc.php') {
-    file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
-    exit;
+/** 載入配置支持*/
+如果（！defined（'__ TYPECHO_ROOT_DIR__'）&&！@include_once'config.inc.php'）{
+    file_exists（'./ install.php'）嗎？header（'Location：install.php'）：print（'缺少配置文件'）;
+    出口;
 }
 
-/** 初始化组件 */
-Typecho_Widget::widget('Widget_Init');
+/** 初始化組件*/
+Typecho_Widget ::小部件（'Widget_Init'）;
 
-/** 注册一个初始化插件 */
-Typecho_Plugin::factory('index.php')->begin();
+/** 註冊一個初始化插件*/
+Typecho_Plugin :: factory（'index.php'）-> begin（）;
 
-/** 开始路由分发 */
-Typecho_Router::dispatch();
+/** 開始路由分發*/
+Typecho_Router :: dispatch（）;
 
-/** 注册一个结束插件 */
-Typecho_Plugin::factory('index.php')->end();
+/** 註冊一個結束插件*/
+Typecho_Plugin :: factory（'index.php'）-> end（）;
